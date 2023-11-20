b24f19f1ea59435429a2bf4c62a6378c600379be
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-11-20 11:27:15 +0100

    Implemented flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,8 @@
 class MainController < ApplicationController
-    def index
-
-    end
+  def index
+    flash[:notice] = "Hey, you are in the home page of our app, enjoy information in this form in entire app!"
+    flash[:alert] = "In this form alerts will be showing themself!"
+    flash[:success] = "In this form success messages will be showing themself!"
+    flash[:error] = "In this form errors will be showing themself!"
+  end
 end
