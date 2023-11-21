c994ebd7ba10ea777e597d0da13399f3308dd46f
bdaf <kamilkorenkiewiczpw@gmail.com>
2023-11-21 16:09:20 +0100

    Simple REGEX for email added in constant_data.rb

--- app/models/constant_data.rb
+++ app/models/constant_data.rb
@@ -1,3 +1,4 @@
 class ConstantData
     VALID_EMAIL_REGEX= /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i
+    SIMPLE_VALID_EMAIL_REGEX = /\A|^@\s]+@[^@\s]+\z/  # [^@\s]+@[^@\s]+
 end
